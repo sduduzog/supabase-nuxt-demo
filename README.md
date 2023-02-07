b8ac6cd788337a86e1b639868c35ca2eb1d8a643
sduduzog <48126337+sduduzog@users.noreply.github.com>
2023-02-07 09:17:33 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# supabase-nuxt-demo
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-kxzrm8)
---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <button>Button</button>
+    <pre>{{ data }} </pre>
+  </div>
+</template>
+
+<style>
+div {
+  padding: 12rem 3rem;
+}
+</style>
+
+<script setup>
+// const { data } = await useFetch('/api/test');
+const data = { tree: 'house' };
+</script>
